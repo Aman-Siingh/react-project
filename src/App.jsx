51d5bd81e7da4b610e919e4de37d3b01bff93fe2
Aman-Siingh <amansiingh15@gmail.com>
2023-12-27 18:39:43 +0530

    Text

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <div>
+    <h1 className="text-red-600 text-3xl font-bold underline">
       Hello world!
     </h1>
+    <p class = "text-green-600 text-4xl">
+      aman Singh </p>
+    
+      
+  </div>
   )
 }
+
